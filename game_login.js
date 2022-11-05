6550e5022a056417e76a7c89e63cbb43fb5aa535
USERCODEHOST <108839721+USERCODEHOST@users.noreply.github.com>
2022-11-05 15:37:17 -0500

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+// Create function addUser()
+function addUser(){
+  player1_name_input=document.getElementById("player1_name_input").value;
+  player2_name_input=document.getElementById("player2_name_input").value;
+  
+      localStorage.setItem("player1_name_input",player1_name_input);
+      localStorage.setItem("player2_name_input",player2_name_input);
+  
+      window.location="game_page.html";
+}
+
+
